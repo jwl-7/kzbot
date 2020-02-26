570c865a6ed26a3ac35635196ff3c929ca54b309
jwl-7 <lucidrainbows@gmail.com>
2020-02-26 15:45:16 -0500

    Add command parameters

--- cogs/jumppb.py
+++ cogs/jumppb.py
@@ -18,8 +18,8 @@ class JumpPb(commands.Cog):
 
     @commands.command()
     @commands.cooldown(1, 10, commands.BucketType.user)
-    async def jumppb(self, ctx):
-        """!jumppb - Get personal best jumpstats."""
+    async def jumppb(self, ctx, bindtype='nobind'):
+        """!jumppb <bind/nobind> - Get personal best jumpstats."""
         discord_id = str(ctx.author)
         account = self.db.get_account(discord_id)
         if not account:
@@ -27,19 +27,24 @@ class JumpPb(commands.Cog):
                 'Error: You need to register your Steam ID with !setaccount <steam_id>'
             )
 
+        bindtype = bindtype.lower()
+        if not kzapi.valid_search_jumppb(bindtype):
+            return await ctx.send('Error: Invalid search parameters for !jumppb')
+
         steam_id = account[1]
         jumptypes = ''
         distances = ''
         strafes = ''
         for jumptype in kzapi.JUMPTYPES_ALT:
-            data = kzapi.get_jumppb(steam_id, jumptype)
+            data = kzapi.get_jumppb(steam_id, jumptype, bindtype)
             jumptypes += f'{jumptype}\n'
             distances += f"{data[0]['distance']}\n" if data else 'N/A\n'
             strafes += f"{data[0]['strafe_count']}\n" if data else 'N/A\n'
 
         embed = discord.Embed(
             colour=discord.Colour.blue(),
-            title=f'Personal Best Jumpstats'
+            title=f'Personal Best Jumpstats',
+            description=f"Type: {'No Bind' if bindtype == 'nobind' else 'Binded'}"
         )
         embed.set_thumbnail(url=ctx.author.avatar_url)
         embed.add_field(name='Player', value=ctx.author.name, inline=False)
