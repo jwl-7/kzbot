e813ab53a6c3bd589968452445b7acb058e537df
jwl-7 <lucidrainbows@gmail.com>
2020-02-28 02:27:54 -0500

    Remove help commands

--- cogs/info.py
+++ cogs/info.py
@@ -1,6 +1,6 @@
 """Info
 
-This module contains help commands.
+This module contains information commands.
 """
 
 
@@ -28,70 +28,6 @@ class Info(commands.Cog):
             self.bot.user.mentioned_in(message)
         ):
             self.bot.mentions_count += 1
-
-    @commands.command()
-    @commands.cooldown(1, 10, commands.BucketType.channel)
-    async def kzbothelp(self, ctx):
-        """!kzbothelp - DM KZBOT command list."""
-        info_cmds = (
-            '**!apistatus** - Check KZ Global API status.\n'
-            '**!kzbothelp** - DM KZBOT command list.\n'
-            '**!kzbotstatus** - KZBOT stats.\n'
-            '**!ping** - Test BOT latency.'
-            )
-        record_cmds = (
-            '**!jumptop** *<jumptype> <bind/nobind>* - Get top jumpstats.\n'
-            '**!maptop** *<map> <mode> <runtype>* - Get top times for map.\n'
-            '**!recentbans** - Get recent bans.\n'
-            '**!recentwrs** - Get recent WRs.\n'
-            '**!wr** *<map> <mode> <runtype>* - Get WR for map.'
-            )
-        leaderboard_cmds = (
-            '**!top** *<mode> <runtype>* - Get top players on records leaderboard.\n'
-            '**!ranktop** *<mode> <runtype>* - Get top players on points leaderboard.'
-            )
-        pb_cmds = (
-            '**!jumppb** *<bind/nobind>* - Get personal best jumpstats.\n'
-            '**!pb** *<map> <mode> <runtype>* - Get personal best time for map.\n'
-            '**!rank** *<mode> <runtype>* - Get personal rank on points leaderboard.\n'
-            '**!setaccount** *<steam_id>* - Register Steam ID to use PB commands.'
-            )
-        embed = discord.Embed(colour=discord.Colour.green())
-        embed.set_author(
-            name='KZ BOT - Command List',
-            icon_url='https://i.imgur.com/sSqZw6W.png'
-        )
-        embed.add_field(name='Info Commands', value=info_cmds, inline=False)
-        embed.add_field(name='Record Commands', value=record_cmds, inline=False)
-        embed.add_field(name='Leaderboard Commands', value=leaderboard_cmds, inline=False)
-        embed.add_field(name='Personal Best Commands', value=pb_cmds, inline=False)
-        try:
-            await ctx.author.send(embed=embed)
-        except discord.Forbidden:
-            return print(f'[ERROR] Failed to send !help list to {ctx.author.name}')
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def adminhelp(self, ctx):
-        """!adminhelp - DM bot admin command list."""
-        admin_cmds = (
-            '**!adminhelp** - DM BOT admin command list.\n'
-            '**!load** *<name>* - Load extension.\n'
-            '**!unload** *<name>* - Unload extension.\n'
-            '**!reload** *<name>* - Reload extension.\n'
-            '**!restart** - Restart BOT.'
-            )
-
-        embed = discord.Embed(colour=discord.Colour.red())
-        embed.set_author(
-            name='KZ BOT - Admin Command List',
-            icon_url='https://i.imgur.com/sSqZw6W.png'
-        )
-        embed.add_field(name=admin_cmds, value='\u200b')
-        try:
-            await ctx.author.send(embed=embed)
-        except discord.Forbidden:
-            return print(f'[ERROR] Failed to send !adminhelp list to {ctx.author.name}')
 
     @commands.command()
     @commands.cooldown(1, 10, commands.BucketType.channel)
